96dcf669afc843a2628bc12fdc636a0b31beebda
mohammadashrafulislam <ixdell41@gmail.com>
2024-09-15 01:06:28 +0600

    icon added

--- src/Component/Map/Map.jsx
+++ src/Component/Map/Map.jsx
@@ -1,10 +1,9 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { Link } from "react-router-dom";
-import { FaRupeeSign, FaMapMarkerAlt } from "react-icons/fa"; 
-import { IconContext } from "react-icons";
-import 'leaflet/dist/leaflet.css';
-
+import { FaRupeeSign } from "react-icons/fa"; 
+import L from "leaflet";
+import { renderToStaticMarkup } from "react-dom/server"; // To convert React icon to string
 
 // Utility function to extract latitude and longitude from the URL
 const getCoordinatesFromUrl = (url) => {
@@ -19,6 +18,32 @@ const getCoordinatesFromUrl = (url) => {
 
   return null; // Return null if coordinates aren't found
 };
+
+// Convert react-icon (FaMapMarkerAlt) to SVG using renderToStaticMarkup
+const markerIconMarkup = renderToStaticMarkup(
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="34"
+    height="34"
+    viewBox="0 0 24 24"
+    fill="#046307"
+    stroke="white"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="feather feather-map-pin"
+  >
+    <path d="M21 10c0 6.075-9 12-9 12s-9-5.925-9-12a9 9 0 1118 0z" /> {/* Pin outline */}
+    <circle cx="12" cy="10" r="3" /> {/* Circular center */}
+  </svg>);
+
+// Custom Leaflet icon using the rendered SVG
+const customIcon = L.divIcon({
+  className: "custom-marker", // Add a custom class to style the marker if needed
+  html: markerIconMarkup, // Inject the SVG markup into the icon
+  iconSize: [36, 36], // Size of the icon
+  iconAnchor: [12, 24], // Position of the icon anchor
+});
 
 const Map = ({ properties }) => {
   return (
@@ -41,14 +66,17 @@ const Map = ({ properties }) => {
 
         return (
           <Marker
-            position={markerPosition} 
+            position={markerPosition}
             key={item._id}
+            icon={customIcon} // Pass the custom icon here
           >
             <Popup>
               <div className="flex gap-2 bg-white items-center w-[280px] p-2">
-                <img src={item.galleryImages[0]} alt={item.name} className="w-[60px] h-[40px]"/>
+                <img src={item.galleryImages[0]} alt={item.name} className="w-[60px] h-[40px]" />
                 <div className="pr-2">
-                  <Link className="font-semibold" to={`/${item._id}`}>{item.name}</Link>
+                  <Link className="font-semibold" to={`/${item._id}`}>
+                    {item.name}
+                  </Link>
                   <span>
                     <b className="flex gap-1 items-start">
                       <FaRupeeSign />
