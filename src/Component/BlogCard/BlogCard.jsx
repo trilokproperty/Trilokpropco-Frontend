fb8151d19114953728d9f32631e364250fc704f2
trilokproperty <trilokpropertyconsultant@gmail.com>
2024-09-28 16:17:11 +0600

    Update BlogCard.jsx

--- src/Component/BlogCard/BlogCard.jsx
+++ src/Component/BlogCard/BlogCard.jsx
@@ -45,7 +45,7 @@ const BlogCard = (blog) => {
         <h3 className="md:text-2xl text-[20px] font-medium blog-title text-black mb-2">{blog?.blog?.title}</h3>
             <p>{truncateText(stripHtmlTags(blog?.blog?.description), 10)}</p>
       </div>
-      <Link to={`/blog/${formattedTitle}/${blog?.blog?._id}`} > <div className="text-2xl w-[100px] bg-black text-white flex justify-center items-center rounded-full mt-8 ml-auto">
+      <Link to={`/blog/${formattedTitle}/${blog?.blog?._id}`} > <div className="dark-mode-btn text-2xl w-[100px] bg-black text-white flex justify-center items-center rounded-full mt-8 ml-auto">
       <small className="text-[12px]">Explore</small><FiArrowRight />
       </div></Link>
       </div>
