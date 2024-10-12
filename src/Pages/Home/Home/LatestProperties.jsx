9f9024150fd1789e14b3ba2f034fdc346b8edda2
trilokproperty <trilokpropertyconsultant@gmail.com>
2024-10-12 14:37:02 +0600

    Update LatestProperties.jsx

--- src/Pages/Home/Home/LatestProperties.jsx
+++ src/Pages/Home/Home/LatestProperties.jsx
@@ -2,14 +2,15 @@ import { useEffect, useState } from "react";
 import { endPoint } from "../../../Component/ForAll/ForAll";
 import SectionTitle from "../../../Component/ForAll/SectionTitle";
 import 'swiper/css/pagination';
+import 'swiper/css/navigation'; // Import Swiper navigation styles
 import { Swiper, SwiperSlide } from "swiper/react";
 // import required modules
-import { Pagination } from 'swiper/modules';
+import { Pagination, Navigation } from 'swiper/modules'; // Import Navigation module
 import PropertyCard from "../../../Component/PropertyCard/PropertyCard";
 
 const LatestProperties = () => {
     const [properties, setProperties] = useState([]);
-    
+
     console.log(properties);
 
     // Fetch properties from the API
@@ -35,57 +36,71 @@ const LatestProperties = () => {
 
     return (
         <div className="mt-4 mb-24 lg:ml-24 lg:mx-0 mx-5">
-            <SectionTitle value="our Latest properties" />
+            <SectionTitle value="Our Latest Properties" />
 
-            <Swiper
-                spaceBetween={30}
-                pagination={{
-                    clickable: true,
-                }}
-                breakpoints={{
-                    0: {
-                        slidesPerView: 1,
-                    },
-                    350: {
-                        slidesPerView: 1,
-                    },
-                    430: {
-                        slidesPerView: 1,
-                    },
-                    490: {
-                        slidesPerView: 1,
-                    },
-                    550: {
-                        slidesPerView: 1.5,
-                    },
-                    640: {
-                        slidesPerView: 1.6,
-                    },
-                    700: {
-                        slidesPerView: 2,
-                    },
-                    900: {
-                        slidesPerView: 2.5,
-                    },
-                    1000: {
-                        slidesPerView: 2.8,
-                    },
-                    1300: {
-                        slidesPerView: 3.5,
-                    },
-                    1700: {
-                        slidesPerView: 4.5,
-                    },
-                }}
-                modules={[Pagination]}
-                className="mySwiper"
-            >
-                {properties.map(property => (
-                    <SwiperSlide key={property._id} className="mb-16">
-                        <PropertyCard property={property} />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+            {/* Custom navigation buttons */}
+            <div className="relative">
+                <button className="swiper-button-prev-custom absolute left-0 z-10 text-2xl bg-white p-3 rounded-full shadow-lg">
+                    &lt; {/* Left arrow */}
+                </button>
+                <button className="swiper-button-next-custom absolute right-0 z-10 text-2xl bg-white p-3 rounded-full shadow-lg">
+                    &gt; {/* Right arrow */}
+                </button>
+
+                <Swiper
+                    spaceBetween={30}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={{
+                        prevEl: '.swiper-button-prev-custom', // Attach the custom button to Swiper
+                        nextEl: '.swiper-button-next-custom', // Attach the custom button to Swiper
+                    }}
+                    breakpoints={{
+                        0: {
+                            slidesPerView: 1,
+                        },
+                        350: {
+                            slidesPerView: 1,
+                        },
+                        430: {
+                            slidesPerView: 1,
+                        },
+                        490: {
+                            slidesPerView: 1,
+                        },
+                        550: {
+                            slidesPerView: 1.5,
+                        },
+                        640: {
+                            slidesPerView: 1.6,
+                        },
+                        700: {
+                            slidesPerView: 2,
+                        },
+                        900: {
+                            slidesPerView: 2.5,
+                        },
+                        1000: {
+                            slidesPerView: 2.8,
+                        },
+                        1300: {
+                            slidesPerView: 3.5,
+                        },
+                        1700: {
+                            slidesPerView: 4.5,
+                        },
+                    }}
+                    modules={[Pagination, Navigation]} // Include Navigation module
+                    className="mySwiper"
+                >
+                    {properties.map(property => (
+                        <SwiperSlide key={property._id} className="mb-16">
+                            <PropertyCard property={property} />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
         </div>
     );
 };
