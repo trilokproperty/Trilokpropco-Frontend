9f80bb318eae58c4cb396ea6eafe8108a148c09b
trilokproperty <trilokpropertyconsultant@gmail.com>
2024-10-12 16:10:50 +0600

    Update LatestProperties.jsx

--- src/Pages/Home/Home/LatestProperties.jsx
+++ src/Pages/Home/Home/LatestProperties.jsx
@@ -49,37 +49,14 @@ const LatestProperties = () => {
         ref={prevRef}
         className="swiper-button-prev absolute !bg-[#ffffff69] !text-gray-800 p-5 rounded-full left-5 top-1/2 transform -translate-y-1/2 z-20 cursor-pointer"
     >
-        <svg
-            stroke="currentColor"
-            fill="currentColor"
-            strokeWidth="0"
-            viewBox="0 0 320 512"
-            className="!text-[20px]"
-            height="20px"
-            width="20px"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"></path>
-        </svg>
+     <
     </div>
 
     {/* Next Button */}
     <div
         ref={nextRef}
         className="swiper-button-next absolute right-5 top-1/2 !bg-[#ffffff69] !text-gray-800 p-5 rounded-full transform -translate-y-1/2 z-20 cursor-pointer"
-    >
-        <svg
-            stroke="currentColor"
-            fill="currentColor"
-            strokeWidth="0"
-            viewBox="0 0 320 512"
-            className="!text-[20px]"
-            height="20px"
-            width="20px"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"></path>
-        </svg>
+    > >
     </div>
 
 
