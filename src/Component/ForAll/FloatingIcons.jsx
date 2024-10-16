a0cb947537fb1095f541f13c130f9093db4e0512
trilokproperty <trilokpropertyconsultant@gmail.com>
2024-10-16 11:48:50 +0600

    Update FloatingIcons.jsx

--- src/Component/ForAll/FloatingIcons.jsx
+++ src/Component/ForAll/FloatingIcons.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+
+import React, { useEffect, useState } from "react";
 import { FaPhone, FaWhatsapp } from "react-icons/fa6";
+import { endPoint } from "../ForAll/ForAll";
 
 const FloatingIcons = () => {
+  const [footerData, setFooterData] = useState(null);
+console.log(footerData)
+  useEffect(() => {
+    const fetchFooterData = async () => {
+      try {
+        const response = await fetch(`${endPoint}/footer`);
+        const data = await response.json();
+        setFooterData(data[0]);
+      } catch (error) {
+        console.error("Error fetching footer data:", error);
+      }
+    };
+
+    fetchFooterData();
+  }, []);
+  
   return (
     <div style={styles.container}>
       <a
-        href="https://wa.me/yourwhatsappnumber"
+        href={`https://wa.me/${footerData?.contact}`}
         target="_blank"
         rel="noopener noreferrer"
         style={styles.icon}
       >
         <FaWhatsapp style={styles.whatsappIcon}/>
       </a>
-      <a href="tel:yourphonenumber" style={styles.icon}>
+      <a href={`tel:${footerData?.contact}`} style={styles.icon}>
         <FaPhone style={styles.phoneIcon}/>
       </a>
     </div>
