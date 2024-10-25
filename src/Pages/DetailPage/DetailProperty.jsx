917cfb2feb79f110ed4bcfb3c14d70f36b65162b
trilokproperty <trilokpropertyconsultant@gmail.com>
2024-10-25 19:42:21 +0600

    Update DetailProperty.jsx

--- src/Pages/DetailPage/DetailProperty.jsx
+++ src/Pages/DetailPage/DetailProperty.jsx
@@ -645,7 +645,7 @@ const currentUrl = `${window.location.origin}${locationUrl.pathname}`; // Use wi
               Project Overview
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mt-6">
-              <div className="text-center border rounded-2xl detailedProperty text-black">
+              <div className="text-center py-3 border rounded-2xl detailedProperty text-black">
                 <h3 className="md:text-2xl text-[18px]  font-medium uppercase">
                   POSSESSION DATE
                 </h3>
@@ -654,7 +654,7 @@ const currentUrl = `${window.location.origin}${locationUrl.pathname}`; // Use wi
                 </p>
               </div>
 
-              <div className="text-center border rounded-2xl detailedProperty text-black">
+              <div className="text-center py-3 border rounded-2xl detailedProperty text-black">
                 <h3 className="md:text-2xl text-[18px] font-medium uppercase">
                   LAND AREA
                 </h3>
@@ -663,7 +663,7 @@ const currentUrl = `${window.location.origin}${locationUrl.pathname}`; // Use wi
                 </p>
               </div>
 
-              <div className="text-center border rounded-2xl detailedProperty text-black">
+              <div className="text-center py-3 border rounded-2xl detailedProperty text-black">
                 <h3 className="md:text-2xl text-[18px] detailedProperty text-black font-medium uppercase">
                   CONFIGURATION
                 </h3>
@@ -672,7 +672,7 @@ const currentUrl = `${window.location.origin}${locationUrl.pathname}`; // Use wi
                 </p>
               </div>
 
-              <div className="text-center border rounded-2xl">
+              <div className="text-center py-3 border rounded-2xl">
                 <h3 className="md:text-2xl text-[18px] detailedProperty text-black font-medium uppercase">
                   FLAT AREA
                 </h3>
@@ -681,7 +681,7 @@ const currentUrl = `${window.location.origin}${locationUrl.pathname}`; // Use wi
                 </p>
               </div>
 
-              <div className="text-center border rounded-2xl">
+              <div className="text-center py-3 border rounded-2xl">
                 <h3 className="md:text-2xl text-[18px] detailedProperty text-black font-medium uppercase">
                   PRICE RANGE
                 </h3>
@@ -690,7 +690,7 @@ const currentUrl = `${window.location.origin}${locationUrl.pathname}`; // Use wi
                 </p>
               </div>
 
-              <div className="text-center border rounded-2xl">
+              <div className="text-center py-3 border rounded-2xl">
                 <h3 className="md:text-2xl text-[18px] detailedProperty text-black font-medium uppercase">
                   NO. OF BLOCKS
                 </h3>
@@ -699,7 +699,7 @@ const currentUrl = `${window.location.origin}${locationUrl.pathname}`; // Use wi
                 </p>
               </div>
 
-              <div className="text-center border rounded-2xl">
+              <div className="text-center py-3 border rounded-2xl">
                 <h3 className="md:text-2xl text-[18px] detailedProperty text-black font-medium uppercase">
                   ELEVATION
                 </h3>
@@ -708,7 +708,7 @@ const currentUrl = `${window.location.origin}${locationUrl.pathname}`; // Use wi
                 </p>
               </div>
 
-              <div className="text-center border rounded-2xl">
+              <div className="text-center py-3 border rounded-2xl">
                 <h3 className="md:text-2xl text-[18px] detailedProperty text-black font-medium uppercase">
                   NO. OF UNITS
                 </h3>
@@ -717,7 +717,7 @@ const currentUrl = `${window.location.origin}${locationUrl.pathname}`; // Use wi
                 </p>
               </div>
 
- <div className="text-center border rounded-2xl">
+ <div className="text-center py-3 border rounded-2xl">
                 <h3 className="md:text-2xl text-[18px] detailedProperty text-black font-medium uppercase">
                   RERA Reg No.
                 </h3>
@@ -812,10 +812,10 @@ const currentUrl = `${window.location.origin}${locationUrl.pathname}`; // Use wi
       {isPlanModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white rounded-lg overflow-hidden">
-            <button className="absolute top-3 right-3 text-white bg-red-500 rounded-full px-2 py-1" onClick={closePlanModal}>
+            <button className="absolute top-3 right-3 text-white bg-red-500 rounded-full px-2 py-2" onClick={closePlanModal}>
               X
             </button>
-            <img src={selectedPlan?.image} alt={selectedPlan?.planType} className="max-w-full max-h-screen" />
+            <img src={selectedPlan?.image} alt={selectedPlan?.planType} className="max-w-5xl" />
           </div>
         </div>
       )}
